c0bc0841c9a308e781a52839d21e7edc962a8f64
tk-zzz <49810315+tk-zzz@users.noreply.github.com>
2020-09-03 02:33:57 +0900

    MainActivity#onCreateで利用してみる

--- app/src/main/java/com/example/rxapiexample/MainActivity.kt
+++ app/src/main/java/com/example/rxapiexample/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.rxapiexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.rxapiexample.infra.repository.ZipCodeRepository
+import rx.android.schedulers.AndroidSchedulers
+import rx.schedulers.Schedulers
+import rx.subscriptions.CompositeSubscription
 
 class MainActivity : AppCompatActivity() {
+
+    private val subscription = CompositeSubscription()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        subscription.clear()
+        subscription.add(
+            ZipCodeRepository.zipCodeApiClient.getZipCode("618", "0000")
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnNext {
+                    Log.e("test!!", "$it")
+                }
+                .doOnError {
+                    it ->  Log.e("test!!", "${it.message}")
+                }
+                .doOnCompleted {
+                }
+                .subscribe())
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        subscription.unsubscribe()
     }
 }
